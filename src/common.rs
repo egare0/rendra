857c0c627f898757a3a3f05fb859a2cdeca50439
Ceyhun <ceyhun.yildizz0@gmail.com>
2026-07-02 21:13:13 +0300

    refactor(common): move raw, color and error to common

--- src/common.rs
+++ src/common.rs
@@ -6,7 +6,13 @@
 mod device;
 mod surface;
 mod renderer;
+mod error;
+mod color;
+
+pub mod raw;
 
 pub use device::Device;
 pub use surface::Surface;
-pub use renderer::{Renderer, Frame};
+pub use renderer::{Frame, Renderer};
+pub use error::RendraError;
+pub use color::Color;
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
 //! `rendra` is a high-level, ergonomic rendering abstraction over wgpu.
 
 mod common;
-mod error;
-mod color;
+pub use common::{Device, Frame, Renderer, Surface, RendraError, Color, raw};
 
 #[cfg(feature = "3d")]
 pub mod render3d;
-
-pub mod raw;
-
-pub use color::Color;
-pub use common::{Device, Surface, Renderer, Frame};
-pub use error::RendraError;
